5ed4fbbb5642e8efe8467ba50cafa8298a42ed59
tjjjwxzq <aquietimmanence@gmail.com>
2016-10-31 23:31:01 +0800

    Remove Flask-Bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from functools import wraps
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -11,7 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app, session_options={'autoflush': False})
 login_manager = LoginManager()
 login_manager.init_app(app)
-bcrypt = Bcrypt(app)
 
 from app.views import pages
 from app.views import customer
