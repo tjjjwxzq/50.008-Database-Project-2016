f77f1ff776441a0e0e87e59212cb66eb05143ee5
tjjjwxzq <aquietimmanence@gmail.com>
2016-10-31 23:32:08 +0800

    Finish store manager login and logout views and templates

--- app/views/store_manager.py
+++ app/views/store_manager.py
@@ -1,14 +1,36 @@
-from flask import Blueprint, render_template, redirect, flash, url_for
-from flask_login import login_user, login_required, logout_user, current_user
-from app import login_manager
+from functools import wraps
+from flask import Blueprint, render_template, redirect, flash, url_for, session
+from app import app
 from app.forms import StoreManagerLoginForm
 from app.models import StoreManager
 
 mod = Blueprint('store_manager', __name__, url_prefix='/store_manager')
 
-@login_manager.user_loader
+# Login helpers, because FLask Login doesn't support multiple user models
+
+def store_manager_logged_in():
+    return session.get('store_manager', None) != None
+
+app.jinja_env.globals.update(store_manager_logged_in=store_manager_logged_in)
+
 def load_store_manager(store_manager_username):
     return StoreManager.query.get(store_manager_username)
+
+def login_required(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        username = session.get('store_manager')
+        if username:
+            store_manager = load_store_manager(username)
+            if store_manager:
+                return function(*args, **kwargs)
+            else:
+                flash('Store manager no longer exists')
+                return redirect(url_for('store_manager.login'))
+        else:
+            flash('Please log in to access this page')
+            return redirect(url_for('store_manager.login'))
+    return wrapper
 
 @mod.route('/login', methods=['GET', 'POST'])
 def login():
@@ -18,11 +40,20 @@ def login():
     if form.validate_on_submit():
         store_manager = StoreManager.query.get(form.username.data)
 
-        if store_manager.verify_password(form.password.data) and login_user(store_manager):
-            flash('Logged in successfullY!')
+        if store_manager.verify_password(form.password.data):
+            session['store_manager'] = store_manager.username
+            flash('Logged in successfully!')
+            print(session)
 
             return redirect(url_for('pages.index'))
         else:
             flash('Failed to log in. Username or password was incorrect.')
 
     return render_template('store_manager/login.html', form=form)
+
+@mod.route('/logout')
+@login_required
+def logout():
+    session.pop('store_manager', None)
+    flash('Logged out successfully!')
+    return redirect(url_for('store_manager.login'))
