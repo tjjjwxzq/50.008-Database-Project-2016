c04e425aff322eee564f9342ac257ab2278bd380
tjjjwxzq <aquietimmanence@gmail.com>
2016-11-02 23:16:36 +0800

    Add book seeds

--- seeds.py
+++ seeds.py
@@ -19,10 +19,23 @@ delete_store_manager = text(
     'DELETE FROM store_manager'
 )
 
+insert_books = text(
+    """INSERT INTO book ("ISBN", title, authors, publisher, year_of_publication, stock, price, format, subject, keywords) VALUES
+    ('9780321197849', 'An Introduction to Database Systems', '{"C.J. Date"}', 'Pearson', 2003, 5, 206.42, 'hardcover', 'Database', '{"database", "computer science"}'),
+    ('9781783555130', 'Python Machine Learning', '{Sebastian Raschka}', 'Packt Publishing', 2015, 10, 40.49, 'softcover', 'Machine Learning', '{"machine learning", "data science", "computer science", "statistical learning"}'),
+    ('9781461471370', 'An Introduction to Statistical Learning: with Applications in R', '{"Gareth James", "Daniela Witten", "Trevor Hastie", "Robert Tibshirani"}', 'Springer', 2013, 2, 72.62, 'hardcover', 'Statistical Learning', '{"machine learning", "data science", "computer science", "statistical learning"}')"""
+)
+
+delete_books = text(
+    'DELETE FROM book'
+)
+
 def run():
     db.engine.execute(insert_customer)
     db.engine.execute(insert_store_manager)
+    db.engine.execute(insert_books)
 
 def clear():
     db.engine.execute(delete_customer)
     db.engine.execute(delete_store_manager)
+    db.engine.execute(delete_books)
