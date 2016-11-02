355da28a6bcecfbb5a6e39eb734e413e2b7a3813
tjjjwxzq <aquietimmanence@gmail.com>
2016-11-02 17:30:27 +0800

    Finish store manager edit book view, edit book form, and template

--- app/forms.py
+++ app/forms.py
@@ -49,3 +49,20 @@ class NewBookForm(FlaskForm):
     format = SelectField('Format', choices=[('hardcover', 'Hardcover'), ('softcover', 'Softcover')])
     subject = StringField('Subject', validators=[DataRequired()])
     keywords = StringField('Keywords', validators=[DataRequired()])
+
+class EditBookForm(FlaskForm):
+    ISBN = StringField('ISBN', validators=[DataRequired(),
+                                           Length(min=13,max=13, message='ISBN should be 13 numbers long'),
+                                           Regexp(r'\d{13}', message='ISBN should consist only of numeric characters')])
+    title = StringField('Title', validators=[DataRequired()])
+    authors = StringField('Authors', validators=[DataRequired()])
+    publisher = StringField('Publisher', validators=[DataRequired()])
+    year_of_publication = IntegerField('Year of Publication', validators=[DataRequired(),
+                                                                         NumberRange(min=1900, max=3000, message='Publication year should be a valid year')])
+    stock = IntegerField('Stock', validators=[InputRequired(),
+                                              NumberRange(min=0, message='Stock should be a valid number')])
+    price = DecimalField('Price', validators=[DataRequired(),
+                                              NumberRange(min=0, message='Price should be a valid number')])
+    format = SelectField('Format', choices=[('hardcover', 'Hardcover'), ('softcover', 'Softcover')])
+    subject = StringField('Subject', validators=[DataRequired()])
+    keywords = StringField('Keywords', validators=[DataRequired()])
