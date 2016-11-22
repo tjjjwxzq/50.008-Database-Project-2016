7dd03be6c34d9d0a47bea3ed33abe6c32da15fbe
tjjjwxzq <aquietimmanence@gmail.com>
2016-11-22 13:14:41 +0800

    Add command to list routes

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask import url_for
+import urllib
 
 from app import app, db
 from seeds import run, clear
@@ -18,6 +20,23 @@ def seed():
 def unseed():
     print('Clearing seed data...')
     clear()
+
+@manager.command
+def routes():
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = '<{0}>'.format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.parse.unquote('{:50s} {:50s} {}'.format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print(line)
 
 if __name__ == '__main__':
     manager.run()
